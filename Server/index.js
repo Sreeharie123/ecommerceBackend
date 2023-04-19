11f4399f062889015a096c1c410934c29fdb5fd3
Sreeharie123 <sreehariengoli@gmail.com>
2023-04-19 16:21:12 +0530

    config the gitignore and dotenv

--- Server/index.js
+++ Server/index.js
@@ -1,33 +1,24 @@
-
-//import the express library 
-const express=require('express')
-const app=express()
-
+//import the express library
+const express = require("express");
+const app = express();
 //import the mongoose library
-const mongoose=require('mongoose')
-//Connect the database
-mongoose.connect("mongodb+srv://sreehari:c5iFWjdqMfB9EuW0@cluster0.kvba6c4.mongodb.net/shop").then((data)=>{
-    console.log("Database connected")
-}).catch(error=>{console.log(error)})
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const mongoose = require("mongoose");
+//import the dotenv library
+const dotenv = require("dotenv");
+//config dotenv
+dotenv.config();
+//Connect the databas
+mongoose
+  .connect(process.env.URL)
+  .then((data) => {
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 //listening the port 4000
-app.listen(4000,()=>{
-    console.log("Server running on port 4000 ")
-}) 
+app.listen(process.env.PORT || 4000, () => {
+  console.log("Server running on port 4000 ");
+});
+

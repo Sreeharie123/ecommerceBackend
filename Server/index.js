549f7712a16fcadfd7539ccb1feda7f32f3fd352
Sreeharie123 <sreehariengoli@gmail.com>
2023-04-23 10:44:52 +0530

    models created

--- Server/index.js
+++ Server/index.js
@@ -1,3 +1,4 @@
+const user=require('./Router/user')
 //import the express library
 const express = require("express");
 const app = express();
@@ -7,6 +8,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 //config dotenv
 dotenv.config();
+app.use(express.json())
 //Connect the databas
 mongoose
   .connect(process.env.URL)
@@ -16,9 +18,10 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
-
+  app.use('/api/users',user)
 //listening the port 4000
 app.listen(process.env.PORT || 4000, () => {
   console.log("Server running on port 4000 ");
 });
-
+// app.use(user)  
+ 

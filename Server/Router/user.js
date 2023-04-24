2e8c0621675ea454de83f93d152814aa7bd83bdc
Sreeharie123 <sreehariengoli@gmail.com>
2023-04-25 01:35:31 +0530

    jwt token function updated the fix bug

--- Server/Router/user.js
+++ Server/Router/user.js
@@ -1,15 +1,17 @@
 const router=require('express').Router();
 const User=require('../models/User')
 const {verifyToken,verifyTokenAndAuthorization }=require('../Router/verify')
+const cryptoJs=require('crypto-js')
 
 router.put('/:id',verifyTokenAndAuthorization,async(req,res)=>{
-    
-  if(req.body.password){
-    req.body.password = CryptoJS.AES.encrypt(
-        req.body.password,
-        process.env.JWT_KEY
-    ).toString()
-  }
+
+    if(req.body.password){
+        const encryptPassword = cryptoJs.AES.encrypt(
+            req.body.password,
+            process.env.KEY
+          ).toString()
+          req.body.password=encryptPassword
+    }
 
   try {
     const updatedUser= await User.findByIdAndUpdate(req.params.id,{

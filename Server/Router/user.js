698143f52edbc29853cf0e38a292bb7065304848
Sreeharie123 <sreehariengoli@gmail.com>
2023-04-24 22:33:54 +0530

    jwt token function is completed

--- Server/Router/user.js
+++ Server/Router/user.js
@@ -1,8 +1,26 @@
 const router=require('express').Router();
-const verifyToken=require('../Router/verifyToken')
+const User=require('../models/User')
+const {verifyToken,verifyTokenAndAuthorization }=require('../Router/verify')
 
-router.put('/:id',verifyToken,(req,res)=>{
+router.put('/:id',verifyTokenAndAuthorization,async(req,res)=>{
+    
+  if(req.body.password){
+    req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.JWT_KEY
+    ).toString()
+  }
 
+  try {
+    const updatedUser= await User.findByIdAndUpdate(req.params.id,{
+        $set:req.body
+    },{new:true})
+    res.status(200).json(updatedUser)
+     
+  } catch (error) {
+    res.status(500).json(error)
+  } 
+   
 })
 
 module.exports = router

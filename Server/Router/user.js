1ddfdde0668df6a68aa69be8fbb47abe4e20d2bb
Sreeharie123 <sreehariengoli@gmail.com>
2023-04-26 09:57:47 +0530

    Admin get user function will be completed

--- Server/Router/user.js
+++ Server/Router/user.js
@@ -1,6 +1,6 @@
-const router=require('express').Router();
+const router=require('express').Router()
 const User=require('../models/User')
-const {verifyToken,verifyTokenAndAuthorization }=require('../Router/verify')
+const {verifyTokenAndAuthorization,verifyTokenAndAdmin}=require('../Router/verify')
 const cryptoJs=require('crypto-js')
 
 router.put('/:id',verifyTokenAndAuthorization,async(req,res)=>{
@@ -24,5 +24,42 @@ router.put('/:id',verifyTokenAndAuthorization,async(req,res)=>{
   } 
    
 })
+
+//Delete
+router.delete('/:id',verifyTokenAndAuthorization,async(req,res)=>{
+  try {
+
+    User.findOneAndDelete(req.params.id)
+    res.status(200).json("User has been deleted")
+    
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+//Get User
+router.get('/find/:id',verifyTokenAndAdmin,async(req,res)=>{
+   try {
+    const user=await User.findById(req.params.id)
+    const{password,...rest}=user._doc
+    res.status(200).json(rest)
+    
+   } catch (error) {
+      res.status(500).json(error)
+   }
+})
+
+//Get AllUsers
+router.get('/all',async(req,res)=>{
+  try {
+    const users= await User.find()
+
+    res.status(200).json(users)
+    
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
 
 module.exports = router

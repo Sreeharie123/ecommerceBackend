d2be196380d0b51d6ba668fb614bef2b59df2a3f
Sreeharie123 <sreehariengoli@gmail.com>
2023-04-26 19:09:59 +0530

    searching and sorting by category

--- Server/Router/product.js
+++ Server/Router/product.js
@@ -41,16 +41,27 @@ router.delete('/:id',verifyTokenAndAdmin,async (req,res)=>{
 })
 
 //Get product
-router.get('/all',async(req,res)=>{
- 
+router.get('/',async(req,res)=>{
+    const qNew=req.query.new;
+    const qCategory=req.query.category; 
     try {
-        const allProduct=await productModel.find()
-        res.status(200).json(allProduct)
+      let products;
+      if(qNew){
+         products=await productModel.find().sort({createdAt:-1}).limit(qNew)
+      }else if(qCategory){
+         products= await productModel.find({categories:{
+            $in:[qCategory]
+         }})
+      } else{
+        products= await productModel.find()  
+      }
 
-    } catch (error) {
+      res.status(200).json(products)
+
+    } catch (error) { 
         res.status(500).json(error)
     }
-
+ 
 })
 
 //Get single product
